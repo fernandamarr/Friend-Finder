19b6de0d84a33bedb3b7673cc8d3dcf8d62383f5
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-02-20 16:43:39 -0500

    fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,11 +11,11 @@ module.exports = function(app) {
     
     // Route to survey page
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // Default to home if no matching route is found
     app.get(function(req,res) {
-        res.sendFile(path.join(_dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
