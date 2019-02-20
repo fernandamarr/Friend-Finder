ff3f8ec6a5f86ab05b6ba48817cd8cee2717ba28
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-02-20 16:33:27 -0500

    fix bugs

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ require("./app/routing/htmlRoutes")(app);
 
 // Listener - "starts" server
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
