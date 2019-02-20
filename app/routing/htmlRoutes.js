675d6315cfdda8396cf8f7016160a793bcaf32fa
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-02-20 17:02:34 -0500

    Add css

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,20 +2,20 @@
 // Path package to get the correct file path for html
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    // Home page route
-    // app.get("/", function(req,res) {
-    //     res.sendFile(path.join(_dirname, "../public/home.html"))
-    // });
-    
+    // Route that sends user to home page
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+
     // Route to survey page
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
     // Default to home if no matching route is found
-    app.get("*", function(req,res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static('app')); //serve images, CSS files, and JavaScript files in the app directory, to be able to load the files that are in the public directory (http://localhost:8080/data/friends.js)
+app.use(express.static(__dirname + "/")); //serve images, CSS files, and JavaScript files in the app directory, to be able to load the files that are in the public directory (http://localhost:8080/data/friends.js)
 app.use(express.urlencoded({
   extended: true
 }));
@@ -23,7 +23,9 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// Listener - "starts" server
+// Starts server to listen
 app.listen(PORT, function () {
   console.log("App listening on http://localhost:" + PORT);
 });
+
+
